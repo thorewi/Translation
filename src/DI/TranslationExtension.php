fb578b9582efa106977d7b3d498886298028e650
Tomáš Kudělka <tomas@appsdevteam.com>
2025-11-28 15:12:40 +0000

    Removes nette/reflection dependency
    Removes the `nette/reflection` dependency and replaces its usage with native PHP reflection.
    
    This simplifies the dependency graph and avoids relying on a potentially unnecessary external library.

--- src/DI/TranslationExtension.php
+++ src/DI/TranslationExtension.php
@@ -39,7 +39,6 @@ use Nette\DI\Helpers;
 use Nette\DI\Statement;
 use Nette\PhpGenerator\ClassType as ClassTypeGenerator;
 use Nette\PhpGenerator\PhpLiteral;
-use Nette\Reflection\ClassType as ReflectionClassType;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
 use Nette\Utils\Finder;
@@ -451,7 +450,7 @@ class TranslationExtension extends \Nette\DI\CompilerExtension
 		try {
 			/** @var \Nette\DI\Definitions\ServiceDefinition $def */
 			$def = $builder->getDefinition($this->loaders[$format]);
-			$refl = ReflectionClassType::from($def->getEntity() ?: $def->getClass());
+			$refl = new \ReflectionClass($def->getEntity() ?: $def->getClass());
 			$method = $refl->getConstructor();
 			if ($method !== NULL && $method->getNumberOfRequiredParameters() > 1) {
 				return;
--- src/Diagnostics/Panel.php
+++ src/Diagnostics/Panel.php
@@ -13,7 +13,6 @@ namespace Kdyby\Translation\Diagnostics;
 use Kdyby\Translation\Translator;
 use Nette\Application\Application;
 use Nette\Application\Request;
-use Nette\Reflection\ClassType;
 use Nette\Utils\Arrays;
 use Nette\Utils\Html;
 use Nette\Utils\Strings;
@@ -273,7 +272,7 @@ class Panel implements \Tracy\IBarPanel
 	{
 		$this->localeResolvers = [];
 		foreach ($resolvers as $resolver) {
-			$this->localeResolvers[ClassType::from($resolver)->getShortName()] = $resolver;
+			$this->localeResolvers[(new \ReflectionClass($resolver))->getShortName()] = $resolver;
 		}
 	}
 
